6cd37664ff54aad3ad6547da5a0817625c1cafc8
clay-jay <youkozel@gmail.com>
2021-05-27 13:09:56 +0300

    stage

--- src/templates/categories.js
+++ src/templates/categories.js
@@ -8,8 +8,23 @@ import HeaderWithIcon from "../components/headers/headerWithIcon"
 import SmartphonesIcon from "../../static/svg/smartphonesIcon.svg"
 import { Grid } from "@material-ui/core"
 import CardWidget from "../components/widgets/cardWidget"
+import { makeStyles} from "@material-ui/core"
+
+const useStyles = makeStyles(theme => ({
+    wrapper: {
+        
+    },
+    cardWidget: {
+        width: "21.895%",
+    },
+    components: {
+        width: "78.105%",
+    }
+
+}))
 
 const Category = ({ data: { prismicCategory } }) => {
+    const classes = useStyles()
   console.log(prismicCategory)
   return (
     <Layout>
@@ -20,12 +35,12 @@ const Category = ({ data: { prismicCategory } }) => {
         title={prismicCategory.data.name}
         divider={false}
       />
-      <Grid>
-          <Grid>
-              <CardWidget variant="categoriesSingle"/>
-
+      <Grid container className={classes.wrapper}>
+          <Grid item className={classes.cardWidget}>
+              <CardWidget variant="categoriesSingle" style={{width: "100%"}}/>
           </Grid>
-          <Grid>
+          <Grid container className={classes.components}>
+
 
           </Grid>
       </Grid>
