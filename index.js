f4475ff53afddbf80eabe8d6095b2e61ce548e72
Pavel <pav38268268@yandex.ru>
2021-07-03 19:16:37 +0300

    fix server script

--- index.js
+++ index.js
@@ -1,36 +1,49 @@
 const express = require("./node_modules/express")
-const { exec } = require("child_process")
+const { exec, execSync } = require("child_process")
 const fs = require("fs")
 const app = express()
 
-app.listen(8888, () => console.log("site deployed to port 8888"))
+app.listen(3999, () => console.log("site deployed to port 3999"))
 
 const build = {
-  status: "Готово",
-  message: "",
+  status: "В ожидании. ",
+  message: "Сборок не было",
 }
 
 app.post("/build", (req, res) => {
-  if (build.status === "Готово") {
-    build.status = "В процессе."
-    build.message = " Cобирается"
+  if (build.status === "В ожидании. ") {
+    build.status = "Собирается. "
     res.sendStatus(200)
-    exec("npm run build", () => {
-      if (fs.existsSync("./public-finish")){
-        fs.renameSync("./public-finish", "./oldVersion")}
 
-      fs.renameSync("./public", "./public-finish")
+    execSync("git pull")
+    execSync("npm install")
+    execSync("npm run clean")
 
-      if (fs.existsSync("./oldVersion")) fs.rmdirSync("./oldVersion")
-
+    exec("npm run build", err => {
       const date = new Date()
+      date.setHours(date.getHours() + 2)
+
       const day = date.getDate()
       const month =
         date.getMonth() < 9 ? "0" + (date.getMonth() + 1) : date.getMonth() + 1
       const hour = date.getHours()
       const min = date.getMinutes()
-      build.status = "Готово"
-      build.message = `. Последнее обновление завершено: ${day}.${month} в ${hour}:${min}`
+
+      if (err) {
+        build.status = "В ожидании. "
+        build.message = `
+         Последнее обновление: ${day}.${month} в ${hour}:${min}. Результат: Ошибка (code:${err.code}, message: ${err.message}). 
+        `
+      } else {
+        fs.renameSync("./public-finish", "./oldVersion")
+        fs.renameSync("./public", "./public-finish")
+        fs.rmSync("./oldVersion", { recursive: true, force: true })
+
+        build.status = "В ожидании. "
+        build.message = `
+          Последнее обновление: ${day}.${month} в ${hour}:${min}. Результат: Успешно.
+        `
+      }
     })
   } else {
     res.sendStatus(409)
