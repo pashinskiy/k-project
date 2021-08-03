157f91559fe2620f7ff6e08b2907d872cf980a37
Pavel <pav38268268@yandex.ru>
2021-08-03 14:33:26 +0300

    fix зфввштп

--- src/components/productPage/delivery/index.js
+++ src/components/productPage/delivery/index.js
@@ -3,7 +3,11 @@ import Grid from "@material-ui/core/Grid"
 import DeliveryCard from "./deliveryCard.js"
 import HeaderWithIcon from "../../headers/headerWithIcon.js"
 import IconDelivery from "../../../../static/svg/deliveryIcon.svg"
-import { useMediaQuery } from "@material-ui/core"
+import { makeStyles, useMediaQuery } from "@material-ui/core"
+
+const useStyle = makeStyles(theme => ({
+  wrapper: { overflow: "hidden" },
+}))
 
 export default function DeliveryCards(prismicProduct) {
   const delivery =
@@ -12,6 +16,8 @@ export default function DeliveryCards(prismicProduct) {
 
   const mobile = useMediaQuery("(max-width: 414px)")
 
+  const classes = useStyle()
+
   return (
     <>
       <HeaderWithIcon
@@ -19,11 +25,7 @@ export default function DeliveryCards(prismicProduct) {
         title="Информация о доставке"
         divider={true}
       />
-      <Grid
-        container
-        spacing={mobile ? 12 : null}
-        style={{ overflow: "hidden" }}
-      >
+      <Grid container spacing={mobile ? 12 : null} className={classes.wrapper}>
         {delivery.length
           ? delivery.map(variant => (
               <Grid item key={variant.city_name}>
