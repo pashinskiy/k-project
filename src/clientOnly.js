cc93b7a182a7309176430bfb153f01997a584420
Pavel <pav38268268@yandex.ru>
2021-08-04 19:26:15 +0300

    add preload

--- src/clientOnly.js
+++ src/clientOnly.js
@@ -1,16 +1,28 @@
 import React from "react"
+import Logo from '../static/svg/logo.svg';
 
 export default function ClientOnly({ children, ...delegated }) {
-  const [hasMounted, setHasMounted] = React.useState(false);
+  const [hasMounted, setHasMounted] = React.useState(false)
   React.useEffect(() => {
-    setHasMounted(true);
-  }, []);
+    setHasMounted(true)
+  }, [])
   if (!hasMounted) {
-    return null;
+    return (
+      <div
+        style={{
+          alignItems: "center",
+          display: "flex",
+          justifyContent: "center",
+          flexWrap: "nowrap",
+          alignContent: "center",
+          height: "100vh",
+          width: "100vw",
+          overflow: "hidden",
+        }}
+      >
+        <Logo />
+      </div>
+    )
   }
-  return (
-    <div {...delegated}>
-      {children}
-    </div>
-  );
+  return <div {...delegated}>{children}</div>
 }
